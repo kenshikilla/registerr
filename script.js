3ea819654b82a14f3f26846f52dd10a6bdf2fcc7
kenshikilla <120762317+kenshikilla@users.noreply.github.com>
2022-12-30 09:29:32 +0600

    help

---
+++ script.js
@@ -0,0 +1,40 @@
+const signUp = e => {
+    let fname = document.getElementById('fname').value,
+        lname = document.getElementById('lname').value,
+        email = document.getElementById('email').value,
+        pwd = document.getElementById('pwd').value;
+
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+
+    let exist = formData.length && 
+        JSON.parse(localStorage.getItem('formData')).some(data => 
+            data.fname.toLowerCase() == fname.toLowerCase() && 
+            data.lname.toLowerCase() == lname.toLowerCase()
+        );
+
+    if(!exist){
+        formData.push({ fname, lname, email, pwd });
+        localStorage.setItem('formData', JSON.stringify(formData));
+        document.querySelector('form').reset();
+        document.getElementById('fname').focus();
+        alert("Аккаунт создан.\n\nПожалуйста войдите по ссылке ниже.");
+    }
+    else{
+        alert("Такой аккаунт уже существует\nВы уже залогинены");
+    }
+    e.preventDefault();
+}
+
+function signIn(e) {
+    let email = document.getElementById('email').value, pwd = document.getElementById('pwd').value;
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+    let exist = formData.length && 
+    JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
+    if(!exist){
+        alert("Некорректно введены данные");
+    }
+    else{
+        location.href = "/";
+    }
+    e.preventDefault();
+}
